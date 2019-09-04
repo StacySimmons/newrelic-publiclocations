84512736afce4813b2302bdc5b4f80e80e4cee02
ssimmons <ssimmons@newrelic.com>
2019-09-04 09:30:21 -0400

    Better exception handling

--- parsePubilcIP.py
+++ parsePubilcIP.py
@@ -10,28 +10,32 @@ args = parser.parse_args()
 output_file = args.outputfile
 endpoint = args.endpoint
 
-response = requests.get(endpoint)
+try:
+    response = requests.get(endpoint)
 
-if response.status_code == requests.codes.ok:
-    print('Success: Public location IP addresses returned')
-    output = ''
+    if response.status_code == requests.codes.ok:
+        print('Success: Public location IP addresses returned')
+        output = ''
 
-    publicLocations_json = response.json()
-    addressCount = 0
+        publicLocations_json = response.json()
+        addressCount = 0
 
-    for location in publicLocations_json:
-        # parse JSON for IP addresses
-        for address in publicLocations_json[location]:
-            output = output + address + '\n'
-            addressCount += 1
+        for location in publicLocations_json:
+            # parse JSON for IP addresses
+            for address in publicLocations_json[location]:
+                output = output + address + '\n'
+                addressCount += 1
 
-print(str(addressCount) + ' IP addresses parsed.')
+        print(str(addressCount) + ' IP addresses parsed.')
 
-file = open(output_file, 'w')
-file.write(output)
-file.close()
+        file = open(output_file, 'w')
+        file.write(output)
+        file.close()
 
-print('IP addresses written to ' + output_file)
-print('Script complete')
+        print('IP addresses written to ' + output_file)
+        print('Script complete')
+    else:
+        print('SCRIPT FAILED: ' + str(response.status_code) + ': ' + response.reason)
 
-
+except Exception as e:
+    print('An exception occurred: ' + str(e))
